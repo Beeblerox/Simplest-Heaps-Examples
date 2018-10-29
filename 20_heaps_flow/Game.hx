56a272204555cceecd1209ed21ab22f3f2405b8b
Zaphod <zaphy@yandex.ru>
2018-10-30 00:34:48 +0300

    added flow example

---
+++ 20_heaps_flow/Game.hx
@@ -0,0 +1,67 @@
+import h3d.mat.Material;
+import hxd.App;
+
+/**
+ * h2d.Flow usage example.
+ * Flow can be seen as flexible container
+ */
+class Game extends hxd.App
+{
+    static function main()
+    {
+        hxd.Res.initEmbed();
+        new Game();
+    }
+
+    var flow:h2d.Flow;
+
+    override function init() 
+    {
+        flow = new h2d.Flow(s2d);
+        // set flow direction
+        // horizontal flow means that elements will placed horizontally until max width is reached
+        flow.isVertical = false;
+        // set max width
+        flow.maxWidth = s2d.width - 100;
+        // set multiline to true, so if total width of added objects is more than max width, then new line will be started
+        flow.multiline = true;
+        flow.x = 0.5 * (s2d.width - flow.maxWidth);
+        flow.y = 50;
+        // set alignment
+        flow.horizontalAlign = Middle;
+        flow.verticalAlign = Top;
+        // set spacing between added objects
+        flow.horizontalSpacing = 10;
+        flow.verticalSpacing = 15;
+        // set padding between objects and outer borders
+        flow.padding = 10;
+
+        // let's add some objects to flow
+        var tile = h2d.Tile.fromColor(0xffffff, 64, 64);
+        for (i in 0...30)
+        {
+            var b = new h2d.Bitmap(tile);
+            b.scaleX = b.scaleY = Math.random() + 1;
+            b.color.set(Math.random(), Math.random(), Math.random());
+            flow.addChild(b);
+        }
+
+        // set flow border, so we can see its actual size
+        flow.backgroundTile = hxd.Res.load("space.png").toImage().toTile();
+        flow.borderWidth = flow.borderHeight = 2;
+
+        // recalculate positions of added objects
+        flow.needReflow = true;
+        flow.reflow();
+    }
+
+    override function update(dt:Float) 
+    {
+        // lets change size of flow object, so we could see it in action
+        if (s2d.mouseX > flow.x + 150)
+        {
+            flow.maxWidth = Std.int(s2d.mouseX - flow.x);
+            flow.reflow();
+        }
+    }
+}
